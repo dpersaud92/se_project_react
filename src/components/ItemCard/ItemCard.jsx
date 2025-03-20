7438c0e4600d5c81a2bfc5591fd13e1cff462c40
dpersaud92 <dxp1020@gmail.com>
2025-03-20 01:22:35 -0400

    Cards configured

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,29 +1,11 @@
 import "./ItemCard.css";
-import Shirt from "../../assets/Shirt.svg";
-import Shorts from "../../assets/Shorts.svg";
-import Cap from "../../assets/Cap.svg";
-import Sneakers from "../../assets/Sneakers.svg";
 
-function ItemCard() {
+function ItemCard({ item }) {
   return (
-    <div className="item__cards">
-      <div className="item__cards-container">
-        <p className="item__cards-name">T-Shirts</p>
-        <img src={Shirt} alt="shirt" className="item__cards-image" />
-      </div>
-      <div className="item__cards-container">
-        <p className="item__cards-name">Shorts</p>
-        <img src={Shorts} alt="shorts" className="item__cards-image" />
-      </div>
-      <div className="item__cards-container">
-        <p className="item__cards-name">Cap</p>
-        <img src={Cap} alt="cap" className="item__cards-image" />
-      </div>
-      <div className="item__cards-container">
-        <p className="item__cards-name">Sneakers</p>
-        <img src={Sneakers} alt="sneakers" className="item__cards-image" />
-      </div>
-    </div>
+    <li className="card">
+      <h2 className="card__name">{item.name}</h2>
+      <img className="card__image" src={item.link} alt={item.name} />
+    </li>
   );
 }
 
