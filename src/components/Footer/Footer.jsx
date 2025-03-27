2daedf09aedf10eebf4ee0ba6f98bf56f2e4f717
dpersaud92 <dxp1020@gmail.com>
2025-03-27 02:02:53 -0400

    Footer configured

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,12 @@
+import "./Footer.css";
+
 function Footer() {
-  return <></>;
+  return (
+    <div className="footer__container">
+      <h2 className="footer__caption">Developed by Dwayne Persaud</h2>
+      <h2 className="footer__year">2025</h2>
+    </div>
+  );
 }
 
 export default Footer;
